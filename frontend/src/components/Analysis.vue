bc113f2758a5f797cb548c7bb306a1d7402c9b35
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2025-05-05 15:07:08 +0200

    Wykres na podstawie danych z backendu

--- frontend/src/components/Analysis.vue
+++ frontend/src/components/Analysis.vue
@@ -1,11 +1,11 @@
 <script>
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
-import { PieChart } from 'echarts/charts'
-import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
+import { BarChart } from 'echarts/charts'
+import { TitleComponent, TooltipComponent, LegendComponent, GridComponent } from 'echarts/components'
 import VChart from 'vue-echarts'
 
-use([ CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent ])
+use([ CanvasRenderer, BarChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent ])
 
 export default {
     props: [ 'session' ],
@@ -14,43 +14,45 @@ export default {
     data() {
         return {
             option: {
-                title: {
-                    text: 'Traffic Sources',
-                    left: 'center',
-                },
-                tooltip: {
-                    trigger: 'item',
-                    formatter: '{a} <br/>{b} : {c} ({d}%)',
-                },
                 legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+                    data: [ 'Liczba pracowników', 'Wiek menedżera' ]
+                },
+                xAxis: {
+                    type: 'category',
+                    data: []
+                },
+                yAxis: {
+                    type: 'value'
                 },
                 series: [
                     {
-                        name: 'Traffic Sources',
-                        type: 'pie',
-                        radius: '55%',
-                        center: ['50%', '60%'],
-                        data: [
-                            { value: 335, name: 'Direct' },
-                            { value: 310, name: 'Email' },
-                            { value: 234, name: 'Ad Networks' },
-                            { value: 135, name: 'Video Ads' },
-                            { value: 1548, name: 'Search Engines' }
-                        ],
-                        emphasis: {
-                            itemStyle: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            }
-                        }
+                        name: 'Liczba pracowników',
+                        data: [],
+                        type: 'bar',
+                        title: 'Liczba pracowników',
+                    },
+                    {
+                        name: 'Wiek menedżera',
+                        data: [],
+                        type: 'bar',
+                        title: 'Wiek menedżera'
                     }
                 ]
             }
         }
+    },
+    mounted() {
+        const queryString = { skip: 0, limit: 10 }
+        fetch('/api/project?' + new URLSearchParams(queryString).toString())
+        .then(res => res.json().then(result => {
+            result.data.forEach(element => {
+                element.managerName = element.manager[0] ? element.manager[0].firstName + ' ' + element.manager[0].lastName : '- bez przydziału -'
+                element.numWorkers = element.worker_ids.length
+            })
+            this.option.xAxis.data = result.data.map(element => element.name)
+            this.option.series[0].data = result.data.map(element => element.numWorkers)
+            this.option.series[1].data = result.data.map(element => element.manager[0].birthDate ? (new Date() - new Date(element.manager[0].birthDate)) / 1000 / 60 / 60 / 24 / 365.25 : 0)
+        }))
     }
 }
 </script>
@@ -61,5 +63,5 @@ export default {
 </template>
 
 <style scoped>
-.chart { height: 100vh; }
+.chart { height: 50vh; }
 </style>
